d5500deb3048d0dc02808ae27d120a61ccacb28d
Bestony <best.tony@foxmail.com>
2016-10-03 22:48:21 +0800

    update hot

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -1,30 +1,31 @@
 Page({
   data:{
-    datas:[]
+    datas:[],
+    hidden:false,
   },
-  
+  onReady:function(){
+    this.setData({
+      hidden:true,
+    })
+  },
+  onShow:function(){
+    
+  },
   onLoad:function(options){
       var that =this;
     wx.request({
         url:"https://www.v2ex.com/api/topics/hot.json",
         success:function(data){
-           // console.info(data.data);
+           
             that.setData({
                 datas:data.data
             })
         }
     })
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  readDetial:function(e){
+    wx.navigateTo({
+      url:"../detail/detail?id="+e.currentTarget.id,
+    })
   }
 })
